2623e264fac345fe2543640d17ece743916a3a6e
jordan_garske <jordago9980@yahoo.com>
2023-05-27 20:37:38 -0500

    updare

--- rustume/src/main.rs
+++ rustume/src/main.rs
@@ -1,8 +1,11 @@
 #[macro_use] extern crate rocket;
-
+use rocket_sync_db_pools::database;
+#[database("my_db")]
+pub struct Db(diesel::PgConnection);
 #[launch]
 fn rocket() -> _ {
     rocket::build()
+    .attach(Db::fairing())
     .mount("/", routes![])
     
 }
